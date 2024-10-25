a2dc918fae89452756637e436097131a553203f7
Joshua Radz Adlaon <joshuaradz.adlaon@g.msuiit.edu.ph>
2024-10-26 01:57:25 +0800

    added mysql db

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
-
+from flask_mysqldb import MySQL
 
 def create_app():
     app = Flask(__name__)
+    
+    app.secret_key = 'joshua'
+    app.config['MYSQL_HOST'] = 'ant-hill-systemproject.e.aivencloud.com'
+    app.config['MYSQL_USER'] = 'avnadmin'
+    app.config['MYSQL_PASSWORD'] = 'AVNS_GqaCqy7Ur5_hs6iLcyk'
+    app.config['MYSQL_DB'] = 'ant_hill'
     
     from .main_route import views
     app.register_blueprint(views)
